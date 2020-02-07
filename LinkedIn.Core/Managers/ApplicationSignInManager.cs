4f4590db0dc64a1e99d39524d60294ec8c4df2e8
MariamZekry <60461396+MariamZekry@users.noreply.github.com>
2020-02-07 22:27:40 +0200

    Revert "Update ApplicationSignInManager.cs"
    This reverts commit e4d7c6b1e8cda6e24e49b6185518f57192107206.

--- LinkedIn.Core/Managers/ApplicationSignInManager.cs
+++ LinkedIn.Core/Managers/ApplicationSignInManager.cs
@@ -1,11 +1,6 @@
-﻿using Linkedin.Models;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin;
-using Microsoft.Owin.Security;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
