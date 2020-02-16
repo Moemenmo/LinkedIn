b7b1452043370d7b4a0cc6c208f10f16c96bf500
Moemenmo <37900927+Moemenmo@users.noreply.github.com>
2020-02-16 23:03:41 +0200

    search

--- LinkedIn.Web/Controllers/HomeController.cs
+++ LinkedIn.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinkedIn.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,15 @@ namespace LinkedIn.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        public UnitOfWork UnitOfWork
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().get<UnitOfWork>();
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -24,6 +34,12 @@ namespace LinkedIn.Web.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult Search(string fname, string lname)
+        {
+            var user = UnitOfWork.ApplicationUserManager;
+            var listOfUsers=user.getall
             return View();
         }
     }
