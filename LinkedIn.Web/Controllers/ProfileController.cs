cbc170a98a35ff09b8d22dc14810460d95934f12
MariamZekry <60461396+MariamZekry@users.noreply.github.com>
2020-02-19 13:32:01 +0200

    add profile control

--- LinkedIn.Web/Controllers/ProfileController.cs
+++ LinkedIn.Web/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace LinkedIn.Web.Controllers
         // GET: Profile
         public ActionResult Index()
         {
+
             return View();
         }
     }
