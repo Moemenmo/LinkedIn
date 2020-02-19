98759c43aca4ea52038b64e8e72cf0d4ba53e2b5
Moemenmo <37900927+Moemenmo@users.noreply.github.com>
2020-02-19 22:26:37 +0200

    update usercard with real data

--- LinkedIn.Web/Controllers/NewsFeedController.cs
+++ LinkedIn.Web/Controllers/NewsFeedController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Linkedin.Models.Entites;
+using LinkedIn.Core;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,33 @@ namespace LinkedIn.Web.Controllers
 {
     public class NewsFeedController : Controller
     {
+        public UnitOfWork UnitOfWork
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<UnitOfWork>();
+            }
+        }
+        public int MyProperty { get; set; }
+        public ApplicationUser loginuser
+        {
+            get
+            {
+                if (loginuser==null)
+                {
+                    return UnitOfWork.ApplicationUserManager.FindById(User.Identity.GetUserId().ToString());
+                }
+    
+                return loginuser;
+            }
+        }
+
+
         // GET: NewsFeed
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            return View(loginuser);
         }
     }
 }
