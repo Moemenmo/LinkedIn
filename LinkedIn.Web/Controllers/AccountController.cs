205897ddae6dd786a9ca95850f69c816b3c2a99f
EnjiBahaa <engiibahaa@gmail.com>
2020-02-27 16:04:16 +0200

    Connection without connect Btn

--- LinkedIn.Web/Controllers/AccountController.cs
+++ LinkedIn.Web/Controllers/AccountController.cs
@@ -527,7 +527,7 @@ namespace LinkedIn.Web.Controllers
 
         }
         public ActionResult Search(string fname, string lname)
-        {
+      {
             List<UserSearchViewModel> searchResults = new List<UserSearchViewModel>();
             var userManager = UnitOfWork.ApplicationUserManager;
             var user = userManager.FindById(User.Identity.GetUserId());
@@ -563,6 +563,15 @@ namespace LinkedIn.Web.Controllers
                 searchResults.Add(temp);
             }
             return View(searchResults);
+        }
+
+        public ActionResult Connect(string id)
+        {
+            //var userManager = UnitOfWork.ApplicationUserManager;
+            //var requestedUser = userManager.FindById(User.Identity.GetUserId());
+            //var user = userManager.FindById(id);
+            //user.Requested(requestedUser);
+            return View();
         }
     }
 
